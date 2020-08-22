03d72d2c8876085fda8a79a731514fd518d3e303
Dauerdaddlah <hertel-dennis@hotmail.com>
2020-08-22 15:56:17 +0200

    corrected index-caluculation

--- src/main/kotlin/de/dde/snes/memory/LoRomMapping.kt
+++ src/main/kotlin/de/dde/snes/memory/LoRomMapping.kt
@@ -6,7 +6,7 @@ class LoRomROMMapping(
     override fun index(bank: Bank, address: ShortAddress): Int {
         // bank and 0x7f is the same as calculating bank - 0x80 if it is above 0x80 -> this way we handle alls mirrors the same
         // same logic for the address itself - and 0x7FFF is the same as calculating - 0x8000 if it is above 0x8000 which handle the upper and lower half the same
-        return (bank and 0x7F * Memory.BANK_SIZE / 2) + (address and 0x7FFF)
+        return ((bank and 0x7F) * Memory.BANK_SIZE / 2) + (address and 0x7FFF)
     }
 }
 
